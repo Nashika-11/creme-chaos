313b0e6ac3dcfc13938d39109652d7ee90960f36
Nashika <nashimeher@gmail.com>
2024-07-15 17:45:11 +0530

    clean code first draft

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -11,4 +11,17 @@ function toggleMobileMenu () {
     }
 }
 
+function closeMenuTab () {
+    mobileMenuPage.classList.remove('active')
+    mobileMenuBtnElement.classList.remove('active')
+
+    for(const menuPageListElement of menuPageListElements){
+        menuPageListElement.classList.remove('active')
+    }
+}
+
 mobileMenuBtnElement.addEventListener('click',toggleMobileMenu)
+
+for(const menuPageListElement of menuPageListElements){
+    menuPageListElement.addEventListener('click',closeMenuTab)
+}
